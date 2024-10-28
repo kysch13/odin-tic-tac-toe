a33e077ab7351e601d1ddc2a9c44d09fe0521786
kysch13 <124356972+kysch13@users.noreply.github.com>
2024-10-27 20:27:30 -0400

    Move placeMarker to a new module

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,31 @@
-console.log('Success');
+const gameBoard = (function () {
+    const squares = [];
+    const buildBoard = function () {
+        for (let x=0; x<3; x++){
+            squares[x] = [];
+            for (let y=0; y<3; y++){
+                squares[x].push(null);
+            }
+        }
+    }
+
+    return {squares, buildBoard};
+})();
+
+const gameProgress = (function () {
+    const placeMarker = function (x, y, marker) {
+        if (gameBoard.squares[x][y] === null) {
+            gameBoard.squares[x][y] = marker;
+        }   
+    }
+    return {placeMarker};
+})();
+
+function createPlayer(name) {
+    return {name};
+}
+
+gameBoard.buildBoard();
+const player1 = createPlayer('Me');
+const player2 = createPlayer('Mac');
+console.log(gameBoard.squares);
